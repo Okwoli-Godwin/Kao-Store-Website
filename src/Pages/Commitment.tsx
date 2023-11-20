06389a65fdcbb6730709de65c794c4e5fac2ad8c
Okwoli-Godwin <okwolig60@gmail.com>
2023-11-20 17:27:56 +0100

    update

--- src/Pages/Commitment.tsx
+++ src/Pages/Commitment.tsx
@@ -1,25 +1,64 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from "styled-components"
 import img from "../Assets/commitmentimg.png"
+import Community from './Dropholder/Community'
 
 const Commitment = () => {
+    const [show, setShow] = useState(false)
+    const [show2, setShow2] = useState(false)
+    const [show3, setShow3] = useState(false)
+    const [show4, setShow4] = useState(false)
+
+    const Toggle = () => {
+        setShow(!show)
+    }
+    const Toggle2 = () => {
+        setShow2(!show2)
+    }
+    const Toggle3 = () => {
+        setShow3(!show3)
+    }
+    const Toggle4 = () => {
+        setShow4(!show4)
+    }
   return (
     <Container>
         <Wrapper>
             <h1>Our <span>Commitment</span></h1>
             <Boxhold>
-                <Box>Community support</Box>
-                <Box>Our Impact</Box>
-                <Box>Donate with JumiaPay</Box>
-                <Box>Sustainability</Box>
+                <Box onClick={Toggle} bg={show ? "" : "bb"}>Community support</Box>
+                <Box2 onClick={Toggle2} bg={show2 ? "bb" : "#fff"}>Our Impact</Box2>
+                <Box3 onClick={Toggle3} bg={show3 ? "bb" : "#fff"}>Donate with JumiaPay</Box3>
+                <Box4 onClick={Toggle4} bg={show4 ? "bb" : "#fff"}>Sustainability</Box4>
             </Boxhold>
+            
         </Wrapper>
+            {show ? (
+                <Holders>
+                    <Community />
+                </Holders>
+            ) : null}
     </Container>
   )
 }
 
 export default Commitment
-const Box = styled.div`
+const Holders = styled.div`
+    width: 100%;
+    height: 200px;
+    background-color: #FFF0E8;
+    bottom: 0;
+    /* margin-bottom: -100px; */
+    /* margin-top: 200px; */
+`
+const Holder = styled.div`
+    width: 100%;
+    height: 200px;
+    position: absolute;
+    bottom: 0;
+    margin-top: 170px;
+`
+const Box4 = styled.div<{bg: string}>`
     width: 250px;
     height: 50px;
     background-color: #fff;
@@ -34,6 +73,55 @@ const Box = styled.div`
     border-top-right-radius: 5px;
     border-top-left-radius: 5px;
     cursor: pointer;
+    background-color: ${({ bg }) => (bg ? "#fff" : "#EBF1F9")};
+`
+const Box3 = styled.div<{bg: string}>`
+    width: 250px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: Ubuntu;
+    font-weight: 500;
+    color: #282828;
+    margin-right: 20px;
+    font-size: 17px;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+    cursor: pointer;
+    background-color: ${({ bg }) => (bg ? "#fff" : "#D0EAF1")};
+`
+const Box2 = styled.div<{bg: string}>`
+    width: 250px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: Ubuntu;
+    font-weight: 500;
+    color: #282828;
+    margin-right: 20px;
+    font-size: 17px;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+    cursor: pointer;
+    background-color: ${({ bg }) => (bg ? "#fff" : "#F0E4F4")};
+`
+const Box = styled.div<{bg: string}>`
+    width: 250px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: Ubuntu;
+    font-weight: 500;
+    color: #282828;
+    margin-right: 20px;
+    font-size: 17px;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+    cursor: pointer;
+    background-color: ${({ bg }) => (bg ? "#fff" : "#FFF0E8")};
 `
 const Boxhold = styled.div`
     display: flex;
@@ -48,6 +136,8 @@ const Wrapper = styled.div`
     padding-top: 165px;
     padding-bottom: 165px;
     position: relative;
+    flex-direction: column;
+    align-items: center;
     h1{
         font-family: Ubuntu;
         color: #fff;
@@ -64,4 +154,7 @@ const Container = styled.div`
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
+    flex-direction: column;
+    /* position: relative; */
+    /* height: 300px; */
 `
---
+++ src/Pages/Dropholder/Community.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Community = () => {
+  return (
+    <Container>
+        <Wrapper>
+            jfjfjfjfj
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Community
+const Wrapper = styled.div`
+    width: 88%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    background-color: #FFF0E8;
+    justify-content: center;
+    padding-top: 30px;
+`
