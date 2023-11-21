8d3ed2d1c922633c09679f83768081091bf28ac4
Okwoli-Godwin <okwolig60@gmail.com>
2023-11-21 13:33:49 +0100

    update

--- src/Pages/Latestnews.tsx
+++ src/Pages/Latestnews.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FaArrowRight } from "react-icons/fa";
 
 const Latestnews = () => {
   return (
@@ -10,19 +11,66 @@ const Latestnews = () => {
             <Firsthold>
                 <Holder>
                     <h3>Starlink and Jumia Collaborate to Expand Internet Service in Africa</h3>
+                    <p>October 2, 2023 | Press Releases</p>
                 </Holder>
+                <Icon><FaArrowRight /></Icon>
             </Firsthold>
+            <Firsthold>
+                <Holder>
+                    <h3>Renaud Glenisson (DG de JUMIA CI): "Désormais Jumia vous livre tout et partout en Côte d’Ivoire" (Publi-info)</h3>
+                    <p>September 15, 2023 | Company News | Ivory Coast</p>
+                </Holder>
+                <Icon><FaArrowRight /></Icon>
+            </Firsthold>
+             <Firsthold>
+                <Holder>
+                    <h3>Jumia’s Ghana CEO is confident he has recipe for profitability</h3>
+                    <p>September 15, 2023 | Company News</p>
+                </Holder>
+                <Icon><FaArrowRight /></Icon>
+            </Firsthold>
+            <Button>Press</Button>
         </Wrapper>
     </Container>
   )
 }
 
 export default Latestnews
+const Button = styled.button`
+    width: 100px;
+    height: 45px;
+    background-color: #0030AD;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    cursor: pointer;
+    font-weight: 500;
+    margin-top: 35px;
+    font-family: Ubuntu;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`
+const Icon = styled.div`
+    margin-top: 3px;
+    color: #0030AD;
+    font-size: 18px;
+`
 const Holder = styled.div`
     display: flex;
     flex-direction: column;
 `
 const Firsthold = styled.div`
+    p{
+        font-family: Ubuntu;
+        font-stretch: normal;
+        font-style: normal;
+        font-weight: 400;
+        letter-spacing: normal;
+        white-space: pre-line;
+        font-size: 16px;
+        line-height: 1.5;
+        margin: 0;
+    }
     h3{
         color: #282828;
         font-family: Ubuntu;
@@ -37,8 +85,10 @@ const Firsthold = styled.div`
     width: 100%;
     padding: 20px;
     border-radius: 10px;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
     align-items: center;
+    justify-content: space-between;
+    margin-bottom: 20px;
 `
 const Line = styled.div`
     width: 40px;
