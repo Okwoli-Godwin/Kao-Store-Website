a6541e476650e8ee9afc3d53efd1a9818435226b
Okwoli-Godwin <okwolig60@gmail.com>
2023-11-24 13:40:07 +0100

    update

--- src/Pages/Latestnews.tsx
+++ src/Pages/Latestnews.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 import { FaTrophy } from "react-icons/fa6";
 import { MdStars } from "react-icons/md";
+import { FiSettings } from "react-icons/fi";
+import { FaHeart } from "react-icons/fa";
+
 
 const Latestnews = () => {
   return (
@@ -18,14 +21,14 @@ const Latestnews = () => {
                 <p>Best Overall rating from more than 2500 reviews accumulated over more than 6 years of hard work and dedication.</p>
             </Firsthold>
             <Firsthold>
-                <Icon><FaTrophy /></Icon>
-                <h3>Best Selling</h3>
-                <p>Since 2013, Journal has been at the top of the Opencart weekly charts for every day, week, month and year in a row.</p>
+                <Icon3><FiSettings /></Icon3>
+                <h3>Tried & Trusted</h3>
+                <p>Years of testing and feedback from our customers have made Journal the most used and tested Opencart theme in the world.</p>
             </Firsthold>
             <Firsthold>
-                <Icon><FaTrophy /></Icon>
-                <h3>Best Selling</h3>
-                <p>Since 2013, Journal has been at the top of the Opencart weekly charts for every day, week, month and year in a row.</p>
+                <Icon4><FaHeart /></Icon4>
+                <h3>Most Loved</h3>
+                <p>with an average 67% satisfaction rate, over 20.000 people have come to love Journal more than any other Opencart theme.</p>
             </Firsthold>
         </Wrapper>
     </Container>
@@ -33,13 +36,21 @@ const Latestnews = () => {
 }
 
 export default Latestnews
+const Icon4 = styled.div`
+    color: red;
+    font-size: 35px;
+`
+const Icon3 = styled.div`
+    color: green;
+    font-size: 35px;
+`
 const Icon2 = styled.div`
     color: #FFFF00;
-    font-size: 35px;
+    font-size: 40px;
 `
 const Icon = styled.div`
     color: #0030AD;
-    font-size: 28px;
+    font-size: 38px;
 `
 const Firsthold = styled.div`
     p{
@@ -54,7 +65,7 @@ const Firsthold = styled.div`
         font-family: Ubuntu;
         font-size: 22px;
     }
-    width: 230px;
+    width: 250px;
     display: flex;
     flex-direction: column;
     align-items: center;
