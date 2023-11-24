4f5de12a2bcf95ffbc4bbc24a578417f3b9b2186
Okwoli-Godwin <okwolig60@gmail.com>
2023-11-24 15:25:39 +0100

    update

--- src/Pages/Digital.tsx
+++ src/Pages/Digital.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import img from "../Assets/digitalimg.png"
+import img from "../Assets/crypto_transafer.png"
 
 const Digital = () => {
   return (
