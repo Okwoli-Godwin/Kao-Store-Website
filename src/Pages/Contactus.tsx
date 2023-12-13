e79d9620212aa5a39247838816e35e97bc82db4c
Okwoli-Godwin <okwolig60@gmail.com>
2023-12-13 06:03:30 +0100

    update

--- src/Pages/Contactus.tsx
+++ src/Pages/Contactus.tsx
@@ -4,16 +4,40 @@ import styled from "styled-components"
 const Contactus = () => {
   return (
     <Container>
-        Contact
+        <Wrapper>
+            <Hold>
+                <Iconhold>
+                    
+                </Iconhold>
+            </Hold>
+        </Wrapper>
     </Container>
   )
 }
 
 export default Contactus
+const Iconhold = styled.div`
+    width: 60px;
+    height: 60px;
+    background-color: #0030AD;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 3px;
+`
+const Hold = styled.div`
+    display: flex;
+    align-items: center;
+`
+const Wrapper = styled.div`
+    width: 88%;
+    display: flex;
+    height: 40px;
+`
 
 const Container = styled.div`
     display: flex;
     width: 100%;
-    height: 30px;
-    background-color: red;
+    justify-content: center;
 `
