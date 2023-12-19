8763b0316ed1d039a8b860e55f783206c9506fa0
Okwoli-Godwin <okwolig60@gmail.com>
2023-12-19 13:45:50 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Routes from './Router/Routes';
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const spinner = document.getElementById("spinner")
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = "none"
+      setLoading(false)
+    }, 4000)
+  }
   return (
     <div>
-      <Routes />
+      {!loading && (
+        <Routes />
+      )}
     </div>
   );
 }
