648ad3666fabd576042f89b822bab314d95c5825
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-08 17:49:26 +0100

    update

--- src/Pages/Hero2.tsx
+++ src/Pages/Hero2.tsx
@@ -1,20 +1,64 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useSpring, animated } from "react-spring"
+
+const gradientColors = ['#0030AD', '#FFDDB7', '#0030AD']
 
 const Hero2 = () => {
+
+   const [gradientProps, setGradient] = useSpring<{
+    colors: string[];
+    offset1: number;
+    offset2: number;
+    offset3: number;
+  }>(() => ({
+    colors: gradientColors,
+    offset1: 0,
+    offset2: 0.5,
+    offset3: 1,
+    config: { duration: 3000 },
+    onRest: () =>
+      setGradient({
+        colors: gradientColors,
+        offset1: 1,
+        offset2: 0.9,
+        offset3: 1,
+        config: { duration: 3150 },
+      }),
+  }));
+    
+  
+
     const myVideo = require("../Assets/bob.mp4");
   return (
     <Container>
         <Video src={myVideo} autoPlay={true} muted playsInline loop={true} />
         
         <Wrapper>
-            <h2>Welcome To The <span>Kao Store</span></h2>
+              <h2>Welcome To The {' '}
+                  <GradientText
+            style={{
+              backgroundImage: gradientProps.colors.interpolate(
+                (color1, color2, color3) =>
+                  `linear-gradient(45deg, ${color1} 0%, ${color2} 50%, ${color3} 100%)`
+              ),
+            }}
+          >
+            Kao Store
+          </GradientText>
+              </h2>
         </Wrapper>
     </Container>
   )
 }
 
 export default Hero2
+const GradientText = styled(animated.span)`
+  font-size: 85px;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
 
 const Video = styled.video`
     width: 100%;
@@ -49,7 +93,7 @@ const Container = styled.div`
     margin-top: -10px;
     height: 100%;
     position: relative;
-    padding-top: 180px;
-    padding-bottom: 180px;
+    padding-top: 190px;
+    padding-bottom: 190px;
     /* background-color: red; */
 `
