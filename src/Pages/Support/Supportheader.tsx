0ada433ea024a23286d7ccf35855da12c4a15224
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-09 16:05:45 +0100

    update

--- src/Pages/Support/Supportheader.tsx
+++ src/Pages/Support/Supportheader.tsx
@@ -4,16 +4,34 @@ import styled from "styled-components"
 const Supportheader = () => {
   return (
     <Container>
-        
+        <Wrapper>
+            <h3>Go to Kao-Store.com</h3>
+            <h2>Kao Store support</h2>
+        </Wrapper>
     </Container>
   )
 }
 
 export default Supportheader
 
+const Wrapper = styled.div`
+    display: flex;
+    width: 90%;
+    align-items: center;
+    h2{
+        font-size: 18px;
+        color: #fff;
+    }
+    h3{
+        color: #ffffffb2;
+        font-size: 16px;
+    }
+`
+
 const Container = styled.div`
     display: flex;
     width: 100%;
     height: 60px;
     background-color: #000000;
+    justify-content: center;
 `
