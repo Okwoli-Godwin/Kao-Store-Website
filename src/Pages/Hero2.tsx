d9d889529175d990bc827fb8cd2d75d24843bf0e
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-09 11:24:01 +0100

    update

--- src/Pages/Hero2.tsx
+++ src/Pages/Hero2.tsx
@@ -1,34 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useSpring, animated } from "react-spring"
-
-const gradientColors = ['#0030AD', '#FFDDB7', '#0030AD']
 
 const Hero2 = () => {
-
-   const [gradientProps, setGradient] = useSpring<{
-    colors: string[];
-    offset1: number;
-    offset2: number;
-    offset3: number;
-  }>(() => ({
-    colors: gradientColors,
-    offset1: 0,
-    offset2: 0.5,
-    offset3: 1,
-    config: { duration: 3000 },
-    onRest: () =>
-      setGradient({
-        colors: gradientColors,
-        offset1: 1,
-        offset2: 0.9,
-        offset3: 1,
-        config: { duration: 3150 },
-      }),
-  }));
     
-  
-
     const myVideo = require("../Assets/bob.mp4");
   return (
     <Container>
@@ -36,16 +10,7 @@ const Hero2 = () => {
         
         <Wrapper>
               <h2>Welcome To The {' '}
-                  <GradientText
-            style={{
-              backgroundImage: gradientProps.colors.interpolate(
-                (color1, color2, color3) =>
-                  `linear-gradient(45deg, ${color1} 0%, ${color2} 50%, ${color3} 100%)`
-              ),
-            }}
-          >
-            Kao Store
-          </GradientText>
+                  <span>Kao Store</span>
               </h2>
         </Wrapper>
     </Container>
@@ -53,18 +18,7 @@ const Hero2 = () => {
 }
 
 export default Hero2
-const GradientText = styled(animated.span)`
-  font-size: 85px;
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-     @media screen and (max-width: 500px) {
-        font-size: 45px;
-    }
-     @media screen and (max-width: 375px) {
-        font-size: 25px;
-    }
-`;
+
 
 const Video = styled.video`
     width: 100%;
@@ -88,8 +42,18 @@ const Wrapper = styled.div`
         @media screen and (max-width: 500px) {
             font-size: 40px;
         }
+        @media screen and (max-width: 320px) {
+            font-size: 28px;
+        }
         span{
             font-size: 75px;
+            background: linear-gradient(94.63deg, #0030AD 10.17%, #FFDDB7 47.09%, #0030AD 83.25%);
+            background-clip: text;
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            @media screen and (max-width: 500px){
+                font-size: 50px;
+            }
         }
     }
 `
