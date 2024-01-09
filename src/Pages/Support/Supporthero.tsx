ad132fa81b39ca405153db2d3a3e4a6399a6b62f
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-09 18:07:07 +0100

    update

--- src/Pages/Support/Supporthero.tsx
+++ src/Pages/Support/Supporthero.tsx
@@ -5,19 +5,52 @@ import img from "../../Assets/support.jpg"
 const Supporthero = () => {
   return (
     <Container>
-        <Wrapper>
-            
+        <Hold>
+            <Wrapper>
+            <h1>Welcome to</h1>
+            <h1>Kao Store Support</h1>
+            <Inputhold>
+            </Inputhold>
         </Wrapper>
+        </Hold>
     </Container>
   )
 }
 
 export default Supporthero
+const Inputhold = styled.div`
+    display: flex;
+    align-items: center;
+    width: 500px;
+    background-color: #fff;
+    height: 45px;
+    margin-top: 25px;
+`
+const Hold = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.3);
+    padding-top: 100px;
+    padding-bottom: 100px;
+`
 const Wrapper = styled.div`
     width: 90%;
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+    h1{
+        font-size: 50px;
+        width: 450px;
+        color: #ffffff;
+        font-weight: 300;
+        text-align: center;
+        letter-spacing: 1px;
+        line-height: 1.1;
+        margin: 0;
+    }
 `
 
 const Container = styled.div`
