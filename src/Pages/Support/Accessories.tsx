0fcd4f49d4be65ad94cbff3f3b82a3782baf75b6
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-09 18:19:38 +0100

    update

---
+++ src/Pages/Support/Accessories.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Accessories = () => {
+  return (
+    <Container>
+        <Wrapper>
+            
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Accessories
+const Wrapper = styled.div`
+    display: flex;
+    width: 90%;
+`
+
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
--- src/Pages/Support/Supportpage.tsx
+++ src/Pages/Support/Supportpage.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Supportheader from './Supportheader'
 import Subheader from './Subheader'
 import Supporthero from './Supporthero'
+import Accessories from './Accessories'
 
 const Supportpage = () => {
   return (
@@ -9,6 +10,7 @@ const Supportpage = () => {
         <Supportheader />
         <Subheader />
         <Supporthero />
+        <Accessories />
     </div>
   )
 }
