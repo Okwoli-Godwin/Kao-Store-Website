8a7ac04e15356412139e56b1fd30567825edb9e4
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-09 16:16:48 +0100

    update

--- src/Pages/Support/Supportheader.tsx
+++ src/Pages/Support/Supportheader.tsx
@@ -6,7 +6,8 @@ const Supportheader = () => {
     <Container>
         <Wrapper>
             <h3>Go to Kao-Store.com</h3>
-            <h2>Kao Store support</h2>
+              <h2>Kao Store <p>support</p></h2>
+              <Button>gffghhhhfhfhfhfhfhfhfh</Button>
         </Wrapper>
     </Container>
   )
@@ -14,13 +15,25 @@ const Supportheader = () => {
 
 export default Supportheader
 
+const Button = styled.div`
+    visibility: hidden;
+`
+
 const Wrapper = styled.div`
     display: flex;
     width: 90%;
     align-items: center;
+    justify-content: space-between;
     h2{
-        font-size: 18px;
+        font-size: 21px;
         color: #fff;
+        width: 130px;
+        text-align: center;
+        margin: 0;
+        p{
+            font-size: 15px;
+            margin: 0;
+        }
     }
     h3{
         color: #ffffffb2;
