12b5ade995abbef5311ecfe07668f01279316282
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-10 08:09:11 +0100

    update

--- src/Pages/Support/Setup.tsx
+++ src/Pages/Support/Setup.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import img from "../../Assets/background.jpg"
 
 const Setup = () => {
   return (
@@ -20,4 +21,9 @@ const Container = styled.div`
     width: 100%;
     display: flex;
     justify-content: center;
+    height: 100vh;
+    background-image: url(${img});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
 `
