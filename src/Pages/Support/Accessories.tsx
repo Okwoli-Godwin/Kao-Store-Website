9c14d96071a60d5b84a26d8a09d609c9b93166ac
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-10 06:11:01 +0100

    update

--- src/Pages/Support/Accessories.tsx
+++ src/Pages/Support/Accessories.tsx
@@ -5,16 +5,32 @@ const Accessories = () => {
   return (
     <Container>
         <Wrapper>
-            
+            <Top>
+                <a href="Home">Home</a>
+            </Top>
         </Wrapper>
     </Container>
   )
 }
 
 export default Accessories
+const Top = styled.div`
+    display: flex;
+    margin-top: 15px;
+    a{
+        color: #737373;
+        cursor: pointer;
+        text-decoration: none;
+        transition: all 350ms ease-in-out;
+        &:hover{
+            color: #000
+        }
+    }
+`
 const Wrapper = styled.div`
     display: flex;
     width: 90%;
+    flex-direction: column;
 `
 
 const Container = styled.div`
