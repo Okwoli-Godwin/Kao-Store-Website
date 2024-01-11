b20de3407b19e00601e41d1b875f189630386c9d
Okwoli-Godwin <okwolig60@gmail.com>
2024-01-11 17:41:23 +0100

    update

--- src/Pages/Support/Faqs.tsx
+++ src/Pages/Support/Faqs.tsx
@@ -61,10 +61,8 @@ const FaqsCardContainer = styled.div<FaqsCardContainerProps>`
     overflow: hidden;
     border-bottom: 1px solid #e5e7eb;
     padding-bottom: 10px;
-    border-radius: 100px;
     padding-left: 30px;
     padding-right: 30px;
-    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
     margin-bottom: 10px;
   }
 
